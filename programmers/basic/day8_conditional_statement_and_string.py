06e99b3318c9bf993381d033309020b36b758ada
노유진 <74805810+YoujinROH@users.noreply.github.com>
2025-11-04 19:12:51 +0900

    feat: add basic day8 problems

--- programmers/basic/day8_conditional_statement_and_string.py
+++ programmers/basic/day8_conditional_statement_and_string.py
@@ -58,56 +58,46 @@ def problem_2(a, b, c, d):
         return min(dice_num)
   
 # -------------------------------------------------
-# [문제 3] 글자 이어 붙여 문자열 만들기 (progress)
-# https://school.programmers.co.kr/learn/courses/30/lessons/181920
-# 📘 설명: 정수 start_num와 end_num가 주어질 때, start_num부터 end_num까지의 숫자를 차례로 담은 리스트를 return하도록 solution 함수를 완성하는 문제  
-# 💡 배운 점: 리스트에 요소 추가할 때 list.append() append 함수 활용하기 
+# [문제 3] 글자 이어 붙여 문자열 만들기
+# https://school.programmers.co.kr/learn/courses/30/lessons/181915
+# 📘 설명: my_string의 index_list의 원소들에 해당하는 인덱스의 글자들을 순서대로 이어 붙인 문자열을 return 하는 solution 함수를 작성하는 문제 
+# 💡 배운 점: for문 활용할 때, out of range나 인덱스에 넘버말고 문자 들어가는 거 주의하기  
 # -------------------------------------------------
 
-def solution(start_num, end_num):
-    result = []
-    for i in range(start_num, end_num+1):
-        result.append(i)
-    return result
+def problem_3(my_string, index_list):
+    answer = ''
+    my_string=list(my_string)
+    for i in range(len(index_list)):
+        answer+=my_string[index_list[i]]
+    return answer
     
 # -------------------------------------------------
 # [문제 4] 9로 나눈 나머지 
-# https://school.programmers.co.kr/learn/courses/30/lessons/181919
-# 📘 설명: 임의의 1,000 보다 작거나 같은 양의 정수 n이 주어질 때 초기값이 n인 콜라츠 수열을 return 하는 solution 함수를 완성하는 문제  
-# 💡 배운 점: 어떤 조건일 때까지 반복일 때는 while문 사용하기 
+# https://school.programmers.co.kr/learn/courses/30/lessons/181914
+# 📘 설명: 음이 아닌 정수가 문자열 number로 주어질 때, 이 정수를 9로 나눈 나머지를 return 하는 solution 함수를 작성하는 문제   
+# 💡 배운 점: input의 type 잘 확인하기. string인지 int인지 
 # -------------------------------------------------
 
-def problem_4(n):
-    answer = []
-    x = n
-    while (x!=1):
-        if x%2==0:
-            answer.append(x)
-            x=x//2
-        else:
-            answer.append(x)
-            x=3*x+1
-    answer.append(1)    
+def problem_4(number):
+    answer = 0
+    answer = int(number)%9
     return answer
 
 # -------------------------------------------------
 # [문제 5] 문자열 여러 번 뒤집기  
-# https://school.programmers.co.kr/learn/courses/30/lessons/181918
-# 📘 설명: 작업을 마친 후 만들어진 stk를 return 하는 solution 함수를 완성하는 문제 
-# 💡 배운 점: 슬라이싱 말고 list.pop()을 사용하면 마지막 원소 제거를 쉽고 메모리 낭비 없이 가능  
+# https://school.programmers.co.kr/learn/courses/30/lessons/181913
+# 📘 설명: my_string에 queries의 명령을 순서대로 처리한 후의 문자열을 return 하는 solution 함수를 작성하는 문제  
+# 💡 배운 점: for i in range(len(a)-1, -1, -1), start, stop, step이고 -1 step으로 0까지 가고 싶으면 하나 더 간 -1을 stop으로 설정해주어야함 
 # -------------------------------------------------
 
-def problem_5(arr):
-    stk = []
-    i = 0
-    while (i<len(arr)):
-        if not stk:
-            stk.append(arr[i])
-            i+=1
-        else:
-            if stk[len(stk)-1] < arr[i]:
-                stk.append(arr[i])
-                i+=1
-            else:
-                stk=stk[0:len(stk)-1]
-    return stk
+def problem_5(my_string, queries): 
+    answer = ''
+    answer = my_string
+    for s,e in queries:
+        tmp_in = ''
+        tmp_out = ''
+        tmp_in = answer[s:e+1]
+        for i in range(len(tmp_in)-1,-1,-1):
+            tmp_out+=tmp_in[i]
+        answer = answer[0:s]+tmp_out+answer[e+1:]
+    return answer

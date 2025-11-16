85a3d9946c7449212a91277acc5c2ed47333551e
노유진 <74805810+YoujinROH@users.noreply.github.com>
2025-11-16 23:16:38 +0900

    feat: add basic day22 problems

--- programmers/basic/day22_function(method)_and_conditional_statement.py
+++ programmers/basic/day22_function(method)_and_conditional_statement.py
@@ -37,7 +37,7 @@ def problem_2(arr1, arr2):
             return 0
   
 # -------------------------------------------------
-# [문제 3] 문자열 변환 
+# [문제 3] 문자열 변환 (progress)
 # https://school.programmers.co.kr/learn/courses/30/lessons/181855
 # 📘 설명: strArr의 원소들을 길이가 같은 문자열들끼리 그룹으로 묶었을 때 가장 개수가 많은 그룹의 크기를 return 하는 solution 함수를 완성하는 문제 
 # 💡 배운 점: 비교 연산자 == 랑 대입 연산자 = 헷갈리지 않기. 딕셔너리 밸류값을 뽑으려면 dict.values() 활용하기 

2c8da2e6742e1cedd1e936af08a66b54d281d8be
노유진 <74805810+YoujinROH@users.noreply.github.com>
2026-01-09 16:04:05 +0900

    feat: add basic day21 problems

--- programmers/basic/day21_function(method)2.py
+++ programmers/basic/day21_function(method)2.py
@@ -1,83 +1,61 @@
 # -------------------------------------------------
 # [문제 1] 뒤에서 5등 위로  
-# https://school.programmers.co.kr/learn/courses/30/lessons/181857
-# 📘 설명: arr의 길이가 2의 정수 거듭제곱이 되도록 arr 뒤에 정수 0을 추가하려고 할 때, arr에 최소한의 개수로 0을 추가한 배열을 return 하는 solution 함수를 작성하는 문제 
-# 💡 배운 점: [0] * n 이런식으로 [0,0,0,0,...] 리스트를 만들어갈 수 있음. 리스트도 문자열처럼 += 로 합칠 수 있음. append 대체로 활용 가능.
+# https://school.programmers.co.kr/learn/courses/30/lessons/181852
+# 📘 설명: 정수로 이루어진 리스트 num_list가 주어졌을 때, num_list에서 가장 작은 5개의 수를 제외한 수들을 오름차순으로 담은 리스트를 return하도록 solution 함수를 완성하는 문제
+# 💡 배운 점: sort() 함수는 따로 변수 저장할 필요 없다는 것 기억하기
 # -------------------------------------------------
 
-def problem_1(arr):
-    answer = 1
-    n = len(arr)
-    
-    while (answer < n):
-        answer*=2
-    
-    arr += [0] * (answer - n)
-    return arr
+def problem_1(num_list):
+    num_list.sort()
+    answer = num_list[5:]
+    return answer
   
 # -------------------------------------------------
 # [문제 2] 전국 대회 선발 고사 
-# https://school.programmers.co.kr/learn/courses/30/lessons/181856
-# 📘 설명: 두 정수 배열 arr1과 arr2가 주어질 때, 위에서 정의한 배열의 대소관계에 대하여 arr2가 크다면 -1, arr1이 크다면 1, 두 배열이 같다면 0을 return 하는 solution 함수를 작성하는 문제 
-# 💡 배운 점: 리스트 안의 요소의 합을 구할 때는 sum() 함수 활용하기 
+# https://school.programmers.co.kr/learn/courses/30/lessons/181851
+# 📘 설명: 각 학생들의 선발 고사 등수를 담은 정수 배열 rank와 전국 대회 참여 가능 여부가 담긴 boolean 배열 attendance가 매개변수로 주어졌을 때, 등수가 높은 순서대로 각각 a, b, c번이라고 할 때 10000 × a + 100 × b + c를 return 하는 solution 함수를 작성하는 문제 
+# 💡 배운 점: sorted()는 무조건 list를 반환. dictionary의 .items()를 뽑으면 dict_items의 객체(튜플들의 묶음). sorted(students.items(), key=lambda x; x[1])는 students.items()를 sort하라! 대신 기준은 x[1]로!
 # -------------------------------------------------
 
-def problem_2(arr1, arr2):
-    if len(arr1)!=len(arr2):
-        if len(arr1) > len(arr2):
-            return 1
-        else:
-            return -1
-    else:
-        if sum(arr1) > sum(arr2):
-            return 1
-        elif sum(arr1) < sum(arr2):
-            return -1
-        else:
-            return 0
+def problem_2(rank, attendance):
+    students = {}
+    for i in range(len(rank)):
+        if attendance[i]:
+            students[i] = rank[i]
+    sorted_students = sorted(students.items(), key=lambda x: x[1])
+    a, b, c = sorted_students[0][0], sorted_students[1][0], sorted_students[2][0]
+    return 10000 * a + 100 * b + c
   
 # -------------------------------------------------
 # [문제 3] 정수 부분 
-# https://school.programmers.co.kr/learn/courses/30/lessons/181855
-# 📘 설명: strArr의 원소들을 길이가 같은 문자열들끼리 그룹으로 묶었을 때 가장 개수가 많은 그룹의 크기를 return 하는 solution 함수를 완성하는 문제 
-# 💡 배운 점: 비교 연산자 == 랑 대입 연산자 = 헷갈리지 않기. 딕셔너리 밸류값을 뽑으려면 dict.values() 활용하기 
+# https://school.programmers.co.kr/learn/courses/30/lessons/181850
+# 📘 설명: 실수 flo가 매개 변수로 주어질 때, flo의 정수 부분을 return하도록 solution 함수를 완성하는 문제
+# 💡 배운 점: 몫 연산자는 //
 # -------------------------------------------------
 
-def problem_3(strArr):
-    answer = {}
-    for word in strArr:
-        if len(word) not in answer:
-            answer[len(word)] = 1
-        else:
-            answer[len(word)] += 1
-    return max(answer.values())
+def problem_3(flo):
+    answer = flo//1
+    return answer
     
 # -------------------------------------------------
 # [문제 4] 문자열 정수의 합 
-# https://school.programmers.co.kr/learn/courses/30/lessons/181854
-# 📘 설명: arr의 길이가 홀수라면 arr의 모든 짝수 인덱스 위치에 n을 더한 배열을, arr의 길이가 짝수라면 arr의 모든 홀수 인덱스 위치에 n을 더한 배열을 return 하는 solution 함수를 작성하는 문제 
-# 💡 배운 점: 인덱스 기준 짝수이면 0, 2, 4,... 인덱스를 의미 
+# https://school.programmers.co.kr/learn/courses/30/lessons/181849
+# 📘 설명: 한 자리 정수로 이루어진 문자열 num_str이 주어질 때, 각 자리수의 합을 return하도록 solution 함수를 완성하는 문제 
+# 💡 배운 점: 문자열 for in range 함수 쓸 때, len으로 하던지 명확히 문자단위로 하되, i를 문자로 고려하던지 하기
 # -------------------------------------------------
 
-def problem_4(arr, n):
-    if len(arr)%2!=0:
-        for i in range(len(arr)):
-            if i%2==0:
-                arr[i]+=n
-    else:
-        for i in range(len(arr)):
-            if i%2!=0:
-                arr[i]+=n          
-    return arr
+def problem_4(num_str):
+    answer = 0
+    for i in range(len(num_str)):
+        answer += int(num_str[i])
+    return answer
   
 # -------------------------------------------------
 # [문제 5] 문자열을 정수로 변환하기 
-# https://school.programmers.co.kr/learn/courses/30/lessons/181853
-# 📘 설명: num_list에서 가장 작은 5개의 수를 오름차순으로 담은 리스트를 return하도록 solution 함수를 완성하는 문제 
-# 💡 배운 점: sort() 함수는 반환값이 None이므로 num_list.sort()[:5] 이런식으로 바로 정렬과 인덱싱은 동시에는 불가능 
+# https://school.programmers.co.kr/learn/courses/30/lessons/181848
+# 📘 설명: 숫자로만 이루어진 문자열 n_str이 주어질 때, n_str을 정수로 변환하여 return하도록 solution 함수를 완성하는 문제 
+# 💡 배운 점: str to int를 위해서는 int() 함수를 사용하기
 # -------------------------------------------------
 
-def problem_5(num_list):
-    num_list.sort()
-    return num_list[:5]
-    return answer
+def solution(n_str):
+    return int(n_str) 
